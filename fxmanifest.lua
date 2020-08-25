89fab86aa4681300f644cc90828f26e3efaa27c0
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-25 02:34:02 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+author 'Scott Tots'
+description 'Map blips for NHRP custom stations'
+version '1.0.0'
+
+-- What to run
+client_script "client.lua"
+
+
+server_script "server.lua"
+
